78633a2f964555195d005fde1ea53709c10f0d98
mikhailvik <viktoriia.raseborg@gmail.com>
2026-09-24 07:38:20 +0000

    my fix... remove unused import

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,8 +6,6 @@ understand a complex domain model first.
 """
 from __future__ import annotations
 
-import json  # oanvänd - ruff ska klaga!!!
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
